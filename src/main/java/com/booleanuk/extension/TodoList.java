0f03590fe82a0d0088514bd5d5f387b5258de841
Stian Rusvik <stian.rusvik@no.experis.com>
2024-08-15 15:39:16 +0200

    made test pass for requirement 1 - extension.

--- src/main/java/com/booleanuk/extension/TodoList.java
+++ src/main/java/com/booleanuk/extension/TodoList.java
@@ -1,14 +1,24 @@
 package com.booleanuk.extension;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class TodoList {
     private ArrayList<Item> list;
+
+    public TodoList() {
+        this.list = new ArrayList<>();
+    }
 
     public void addTask(String message) {
         list.add(new Item(message));
     }
 
     public Item getTaskById(String id) {
-        return new Item("test");
+        for (Item listItem : list) {
+            if (Objects.equals(listItem.getId(), id)) {
+                return listItem;
+            }
+        }
+        return null;
     }
 }
